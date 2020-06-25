3fb444a6fec1108f20272b2eafc74fc88d30793e
SHANTHAKUMAR <shanthakmr6@gmail.com>
2020-06-25 10:35:37 +0530

    Initial Commit

---
+++ src/main/java/com/assignment/rabobankcsp/services/validator/ValidatorService.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.assignment.rabobankcsp.services.validator;
+
+import java.util.List;
+
+import com.assignment.rabobankcsp.model.ErrorRecords;
+import com.assignment.rabobankcsp.model.Record;
+
+/**
+ * @author Admin
+ *
+ */
+
+
+public interface ValidatorService {
+
+	public List<ErrorRecords> getDuplicateRecords(List<Record> records);
+	
+	public List<ErrorRecords> getEndBalanceErrorRecords(List<Record> records);	
+}
---
+++ src/main/java/com/assignment/rabobankcsp/services/validator/parser/CustomerStatementParserService.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.assignment.rabobankcsp.services.validator.parser;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import org.json.JSONException;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.assignment.rabobankcsp.model.Record;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonSyntaxException;
+
+/**
+ * @author Admin
+ *
+ */
+public interface CustomerStatementParserService {
+	
+	public List<Record> parseStament(MultipartFile file) throws JsonSyntaxException, JsonIOException, IOException, JSONException;
+	
+	public List<Record> getjsonRecords(File convFile) throws JsonMappingException, JsonProcessingException;
+}
